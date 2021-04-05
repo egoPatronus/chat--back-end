e2dbd2cbb33afa0cc41df2aea9c45fe1ba5abaf7
Guilherme L. Moraes <guilherme.lmoraes32@gmail.com>
2021-04-05 15:00:03 -0300

    chore(repo): added connection to database

---
+++ src/config/database-connection.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const { connect, connection } = mongoose;
+const Logger = require('./logging');
+
+module.exports = function databaseConnection() {
+  connect(process.env.DATABASE_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }).catch(({ message }) => Logger.error(`[MONGODB]: ${message}`));
+
+  const database = connection;
+  database.on('error', ({ message }) => Logger.error(`[MONGODB]: ${message}`));
+  database.once('open', () => {
+    Logger.debug('[MONGODB] Successfully connected');
+  });
+};
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const { Server } = require('http');
 
 const Logger = require('./config/logging');
 const morgan = require('./config/morgan');
+const databaseConnection = require('./config/database-connection');
 
 const PORT = process.env.PORT || 8000;
 
@@ -12,6 +13,7 @@ function bootServer() {
   const app = express();
   app.use(morgan);
   app.use(express.json());
+  databaseConnection();
 
   const server = new Server(app);
   server.listen(PORT, () => Logger.debug(`[EXPRESS] Running on port ${PORT}`));
