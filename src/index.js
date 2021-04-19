48019ceca93562465db3627aa8ebfce6f1a9dfcc
Guilherme L. Moraes <guilherme.lmoraes32@gmail.com>
2021-04-19 18:01:06 -0300

    chore(repo): uploading changes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
+const { Server } = require('socket.io');
 const { createServer } = require('http');
 
 const Logger = require('./config/logging');
 const morgan = require('./config/morgan');
+const jwtVerify = require('./middlewares/jwt-verify');
 const databaseConnection = require('./config/database-connection');
 const router = require('./router');
 
@@ -16,14 +18,19 @@ function bootServer() {
   const app = express();
   app.use(morgan);
   app.use(express.json());
-  app.use(cors({
-    origin: 'http://localhost:3000',
-    credentials: true,
-  }));
+  app.use(cors({ origin: '*' }));
   app.use('/', router);
 
-  const server = createServer(app);
-  server.listen(PORT, () => Logger.debug(`[EXPRESS] Running on port ${PORT}`));
+  const httpServer = createServer(app);
+  const ws = new Server(httpServer, {
+    cors: {
+      origin: '*',
+    },
+  });
+
+  ws.use(jwtVerify);
+  ws.on('connection', (socket) => console.log(`${socket.id} connected`));
+  httpServer.listen(PORT, () => Logger.debug(`[EXPRESS] Running on port ${PORT}`));
 }
 
 bootServer();
--- src/services/user.js
+++ src/services/user.js
@@ -8,16 +8,16 @@ module.exports = function UserService() {
     return user;
   }
 
-  async function validate(email, password) {
-    const user = await User.findOne({ email }, 'email username hash').exec();
+  async function validate(userEmail, password) {
+    const user = await User.findOne({ email: userEmail }, 'email username hash').exec();
     const error = new Error('User does not exist');
     error.name = 'ValidationError';
 
     if (user) {
       const doesPasswordMatch = await user.comparePassword(password);
       if (doesPasswordMatch) {
-        const { username, email: userEmail } = user;
-        return { username, userEmail };
+        const { username, email } = user;
+        return { username, email };
       }
       error.message = 'Password is incorrect';
       throw error;
